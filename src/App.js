5ed8bb2d805d9ca704d89fd608b814af98d6c12b
Srivathsan Baskaran <srivathsan.baskaran@sprinklr.com>
2021-07-19 21:35:18 +0530

    Added a new component with game for new build

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Suspense} from "react";
+import { Suspense } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css"
 
@@ -10,40 +10,45 @@ const CustomerShop = React.lazy(() => import(/* webpackChunkName: "customer-shop
 const VendorShop = React.lazy(() => import(/* webpackChunkName: "vendor-shop" */ './components/vendor-shop'));
 const NewProduct = React.lazy(() => import(/* webpackChunkName: "create-new-product" */ './components/create-new-product'));
 const Results = React.lazy(() => import(/* webpackChunkName: "displayresults" */ './components/displayresults'));
+const Game = React.lazy(() => import(/* webpackChunkName: "Game" */ './components/Game'));
 
 function App() {
   return (
-      <Router>
-        <Suspense fallback={<div>Loading...</div>}>
-            <div className="container">
-              <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <Link to="/" className="navbar-brand">App</Link>
-                <div className="collapse navbar-collapse">
-                  <ul className="navbar-nav mr-auto">
-                    <li className="navbar-item">
-                      <Link to="/" className="nav-link">Users</Link>
-                    </li>
-                    <li className="navbar-item">
-                      <Link to="/create" className="nav-link">Create User</Link>
-                    </li>
-                    <li className="navbar-item">
-                      <Link to="/login" className="nav-link">User Login</Link>
-                    </li>
-                  </ul>
-                </div>
-              </nav>
-
-              <br/>
-              <Route path="/" exact component={UsersList}/>
-              <Route path="/create" component={CreateUser}/>
-              <Route path="/login" component={Login}/>
-              <Route path="/customershop" component={CustomerShop}/>
-              <Route path="/vendorshop" component={VendorShop}/>
-              <Route path="/createproduct" component={NewProduct}/>
-              <Route path="/results" component={Results}/>
+    <Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link to="/" className="navbar-brand">App</Link>
+            <div className="collapse navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">Users</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/create" className="nav-link">Create User</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/login" className="nav-link">User Login</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/game" className="nav-link">Game</Link>
+                </li>
+              </ul>
             </div>
-        </Suspense>
-      </Router>
+          </nav>
+
+          <br />
+          <Route path="/" exact component={UsersList} />
+          <Route path="/create" component={CreateUser} />
+          <Route path="/login" component={Login} />
+          <Route path="/customershop" component={CustomerShop} />
+          <Route path="/vendorshop" component={VendorShop} />
+          <Route path="/createproduct" component={NewProduct} />
+          <Route path="/results" component={Results} />
+          <Route path="/game" component={Game} />
+        </div>
+      </Suspense>
+    </Router>
   );
 }
 
